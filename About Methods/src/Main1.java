e034d85505736dd5194bb68c5812fa3c3ce3017a
Ed Locquiao <nilatian.15@gmail.com>
2022-07-18 21:02:48 -0700

    final revision

--- About Methods/src/Main1.java
+++ About Methods/src/Main1.java
@@ -2,8 +2,37 @@
 public class Main1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String firstName = "Bob";
+		String lastName = "Ross";
+		String fullName = createFullName(firstName, lastName);
+		String fullName2 = createFullName("Sam", lastName);
+		
+		System.out.println(fullName);
+		System.out.println(fullName2);
+		
+		/*
+		 * method signature
+		 * public static Return data type method name (parameters: data type variable name){
+		 * parameters are the values that the method takes and are separated by a comma
+		 * the parameters passed into the method do not have to match as long as the data type
+		 * is the same
+		 * 
+		 * To call a method, use the method name and include in the parameters
+		 * 
+		 */
+	
+		
+		
 	}
-
+	
+	public static String createFullName(String x, String y) {
+		String fullName = x + " " + y;
+		return fullName;
+		// return x + " " + y; is also acceptable  
+	}
+	
+	
+	
 }
+
+
